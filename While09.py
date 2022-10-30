9078c6699c4a2f724539591e392d00f0a3d97d24
turonov <turonov5400gmail.com>
2022-10-31 00:48:02 +0500

    09

--- While09.py
+++ While09.py
@@ -6,4 +6,17 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a = 0
+    b = 0
+    c = 0
+    while a < len(s):
+        if s[a].isdigit():
+            if int(s[a])% 2 == 0:
+                b +=1
+        if s[a].isdigit():
+            if int (s[a])%2 == 0:
+                c +=1
+        a+=1
+    return a
+
+print(main("85858"))
