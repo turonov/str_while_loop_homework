ae9c651c09ddca94cf882941eaa13ab186c40f0b
turonov <turonov5400gmail.com>
2022-10-30 23:50:31 +0500

    04

--- While04.py
+++ While04.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a = 0
+    b = 0
+    while a < len(s):
+        if s[a].isupper():
+            b +=1
+        a +=1  
+    return b
+
+print(main("lldkaAAJDldk"))
