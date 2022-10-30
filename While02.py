f5ac648cfd56f78afaff3baf04d4266a90279e03
turonov <turonov5400gmail.com>
2022-10-30 23:24:24 +0500

    02

--- While02.py
+++ While02.py
@@ -6,4 +6,12 @@ def main(s):
     Returns:
         int: return answer
     """
-    return
+    a = 0
+    b = 0
+    while a < len(s):
+        if s[a].isalpha():
+            b +=1
+        a +=1  
+    return b
+
+print(main("slom0033"))
