bece3fa875bccd0b9abc8e290d2f248284d76c12
turonov <115349142+turonov@users.noreply.github.com>
2022-10-31 00:56:20 +0500

    0220

--- While09.py
+++ While09.py
@@ -9,14 +9,6 @@ def main(s):
     a = 0
     b = 0
     c = 0
-    while a < len(s):
-        if s[a].isdigit():
-            if int(s[a])% 2 == 0:
-                b +=1
-        if s[a].isdigit():
-            if int (s[a])%2 == 0:
-                c +=1
-        a+=1
     return a
 
 print(main("85858"))
