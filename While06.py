9437d5afa06f5dfdc809ccb957a4be2fc18f2142
turonov <turonov5400gmail.com>
2022-10-31 09:44:25 +0500

    066

--- While06.py
+++ While06.py
@@ -11,7 +11,7 @@ def main(s):
     b = 0
     s=s.lower()
     while a < len(s):
-        if s[a] =="a"or s[a]=="e" or s[a] == "i" or s[a]=="o" or s[a]=="u":
+        if s[a] =="a" or s[a]=="e" or s[a] == "i" or s[a]=="o" or s[a]=="u":
             b += 1
         else:
             a += 1
