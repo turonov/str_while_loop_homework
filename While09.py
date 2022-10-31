37d4d4f792ca1691ec1b54c2e9f75dba1ede76e2
turonov <turonov5400gmail.com>
2022-10-31 10:03:37 +0500

    09

--- While09.py
+++ While09.py
@@ -1,14 +1,18 @@
-def main(s):
-    """
-    A string of numbers is given. Find the sum of all the numbers and return.
-    Args:
-        s: str
-    Returns:
-        int: return answer
-    """
-    a = 0
-    b = 0
-    c = 0
-    return a
+# def main(s):
+#     """
+#     A string of numbers is given. Find the sum of all the numbers and return.
+#     Args:
+#         s: str
+#     Returns:
+#         int: return answer
+#     """
+#     a = 0
+#     b = 0
+#     c = 0
+#     while a < len(s):
+#         if s[a].isdigit():
+#             if int(s[a])+
+#         a+=1
+#     return a
 
-print(main("85858"))
+# print(main("85858"))
